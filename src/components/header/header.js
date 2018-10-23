2ab900568cbd953c9cac15b421a13d8b8929c73c
Peter Midthun <petermidthun@gmail.com>
2018-10-23 16:27:03 -0500

    header working

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>The Famous People App</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
