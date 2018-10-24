151c857d6a4248a129c115331d417656e4c01b81
Peter Midthun <petermidthun@gmail.com>
2018-10-24 00:02:25 -0500

    state with name and role on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/header/header';
+import Header from '../header/header';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "name",
+      role: "role"
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <Header />
+
+        {this.state.name} is famous for {this.state.role}
       </div>
     );
   }
